8d006fea28019b2274d23c7f67e7c888b4199b54
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-04-09 12:43:05 -0600

    added dependecies

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Import the THREE.js library
-import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+import * as THREE from 'three';
 // To allow for the camera to move around the scene
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 // To allow for importing the .gltf file
